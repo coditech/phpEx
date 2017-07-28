f9bcdf99f1b51498b100870c411f0c6c7215148a
Xananax <xananax@yelostudio.com>
2017-07-28 12:42:29 +0300

    forms

--- index.php
+++ index.php
@@ -12,13 +12,23 @@ i<!doctype html>
 			print_r($_POST)
 		?>
 		<div id="Wrapper">
+			<form method="post">
+				<input type="text" name="text" value=""/>
+				<input type="hidden" name="action" value="new"/>
+				<input type="submit" name="ok" value="ok"/>
+			</form>
 			<ul>
 				<li>
 					<form method="post">
 						<input type="checkbox" name="done" id="task"/>
 						<label for="task">This is a task</label>
 						<input type="hidden" name="id" value="a"/>
+						<input type="hidden" name="action" value="edit"/>
 						<input type="submit" name="ok" value="ok">
+					</form>
+					<form method="post">
+						<input type="hidden" name="id" value="a"/>
+						<input type="hidden" name="action" value="delete"/>
 						<input type="submit" name="delete" value="delete">
 					</form>
 				</li>
